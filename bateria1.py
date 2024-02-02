16c90f9cc9aca184dd29a16f828c9267802a6fb1
XaSaFa <110727546+XaSaFa@users.noreply.github.com>
2024-02-02 13:20:04 +0100

    Create bateria1.py

---
+++ bateria1.py
@@ -0,0 +1,5 @@
+# Programa que pregunta dos números a l'usuari i calcula la seva suma.
+num1 = input("Introdueix un numero: ")
+num2 = input("Introdueix un altre numero: ")
+suma = int(num1) + int(num2)
+print("El resultat es: ", suma)
